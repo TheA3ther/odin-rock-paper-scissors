16faf76da82afbb20da93d47f34d1ec62b0f7db8
TheA3ther <carlosroycanoperez@gmail.com>
2024-09-29 15:51:29 +0800

    Add ui to rps game

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-let humanScore = 0, computerScore = 0;
+let humanScore = 0, computerScore = 0, hasWinner = false;
+
+const humanChoice = document.querySelector("#humanChoice");
+const resultDisplay = document.querySelector("#result");
+const humanScoreDisplay = document.querySelector("#humanScore");
+const computerScoreDisplay = document.querySelector("#computerScore");
+const computerChoiceDisplay = document.querySelector("#computerChoice");
+const reset = document.querySelector("#reset");
+
+humanChoice.addEventListener('click', (event) => playRound(event.target.id, getComputerChoice()));
+reset.addEventListener('click', resetGame);
 
 function getComputerChoice() {
     let result = Math.random() * 3;
     return (result < 1 ? "rock" : result < 2 ? "paper" : "scissors");
 }
 
-function getHumanChoice() {
-    return (prompt("What do you play? ")).toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
-    console.log(`Computer's Choice: ${computerChoice}`);
+    console.log(hasWinner);
+    computerChoiceDisplay.textContent = computerChoice;
     const result = `${humanChoice},${computerChoice}`;
     switch (result) {
         case ("rock,scissors"):
         case ("paper,rock"):
         case ("scissors,paper"):
-            console.log("You win! One point for you!");
-            humanScore++;
+            resultDisplay.textContent = "Human wins!"
+            humanScoreDisplay.textContent = (++humanScore).toString();
             break;
         case ("scissors,rock"):
         case ("rock,paper"):
         case ("paper,scissors"):
-            console.log("You lose! One point for the computer!");
-            computerScore++;
+            resultDisplay.textContent = "Computer wins!"
+            computerScoreDisplay.textContent = (++computerScore).toString();
             break;
         default:
-            console.log("Tie! Try again!");
+            resultDisplay.textContent = "Tie!"
+    }
+    if ((humanScore == 5 || computerScore == 5) && !hasWinner){    
+        if (humanScore == 5) alert("Human wins!!!");
+        if (computerScore == 5) alert("Computer wins!!!");
+        if (!hasWinner) hasWinner = true;
     }
 }
 
-function playGame(rounds) {
-    for (let i = 0; i < rounds; i++) {
-        console.log(`ROUND #${i + 1}`);
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}!`);
-        console.log(`${rounds - (i + 1)} rounds left!`);
-        console.log("---------------------------------------");
-    }
-    console.log("RESULT");
-    if (humanScore > computerScore) {
-        console.log("Human wins!");
-    }
-    else if (computerScore < humanScore) {
-        console.log("Computer wins!");
-    }
-    else {
-        console.log("It's a tie!");
-    }
+function resetGame(){
+    hasWinner = false;
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+    computerChoiceDisplay.textContent = '';
+    result.textContent = '';
 }
-
-playGame(5);
